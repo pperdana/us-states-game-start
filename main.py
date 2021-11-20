088ee96203d69e21d215cc168eb3231ab22b16a4
Panji <panji9502@gmail.com>
2021-11-20 10:00:55 +0700

    record missing guess to csv file

--- main.py
+++ main.py
@@ -8,27 +8,36 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
-state = data["state"].tolist()
+states = data["state"].tolist()
 text = turtle.Turtle()
 text.color("black")
 text.hideturtle()
 text.penup()
 
 
-# record the correct state
-correct_list = []
+# record the correct and missing state
+correct_states = []
+miss_states = []
 
-while len(correct_list) < 50:
-    answer_state = screen.textinput(title=f"{len(correct_list)}/50 States Correct",
+while len(correct_states) < 50:
+    answer_state = screen.textinput(title=f"{len(correct_states)}/50 States Correct",
                                     prompt="What is another state's name").title()
+    if answer_state == "Exit":
+        break
 
-    if answer_state in state and answer_state not in correct_list:
+    if answer_state in states and answer_state not in correct_states:
         x_pos = int(data[data.state == answer_state].x)
         y_pos = int(data[data.state == answer_state].y)
         text.goto(x_pos, y_pos)
         text.write(answer_state, font=("Courier", 8, "normal"))
 
-        correct_list.append(answer_state)
+        correct_states.append(answer_state)
+
+for state in states:
+    if state not in correct_states:
+        miss_states.append(state)
+
+s = pandas.Series(miss_states)
+s.to_csv("Missing states.csv")
 
 # print(correct_list)
-screen.mainloop()
