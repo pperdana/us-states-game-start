c7b79878bfc6d87f8ff37c0bd82676fac7828775
Panji <panji9502@gmail.com>
2021-11-20 09:41:33 +0700

    record correct guess

--- main.py
+++ main.py
@@ -8,19 +8,27 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
-
-answer_state = screen.textinput(title="Guess the state", prompt="What is another state's name").title()
-text = turtle.Turtle()
-
 state = data["state"].tolist()
-
-if answer_state in state:
-    x_pos = int(data[data.state == answer_state].x)
-    y_pos = int(data[data.state == answer_state].y)
-    text.color("black")
-    text.hideturtle()
-    text.goto(x_pos, y_pos)
-    text.write(answer_state, font=("Courier", 8, "normal"))
+text = turtle.Turtle()
+text.color("black")
+text.hideturtle()
+text.penup()
 
 
+# record the correct state
+correct_list = []
+
+while len(correct_list) < 50:
+    answer_state = screen.textinput(title=f"{len(correct_list)}/50 States Correct",
+                                    prompt="What is another state's name").title()
+
+    if answer_state in state and answer_state not in correct_list:
+        x_pos = int(data[data.state == answer_state].x)
+        y_pos = int(data[data.state == answer_state].y)
+        text.goto(x_pos, y_pos)
+        text.write(answer_state, font=("Courier", 8, "normal"))
+
+        correct_list.append(answer_state)
+
+# print(correct_list)
 screen.mainloop()
